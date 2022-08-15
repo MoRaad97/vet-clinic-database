f67a4d063504d6e866ed930040566d0e543ff5c7
MoRaad97 <89518488+MoRaad97@users.noreply.github.com>
2022-08-15 12:49:52 +0300

    Add  Required Queries.

--- queries.sql
+++ queries.sql
@@ -8,3 +8,11 @@ SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
 SELECT * FROM animals WHERE neutered = true;
 SELECT * FROM animals WHERE name <> 'Gabumon';
 SELECT * FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
+
+--part 2
+ Select COUNT(*) AS number_of_animals FROM animals;
+ Select escape_attempts, COUNT(*) FROM animals GROUP BY escape_attempts HAVING escape_attempts = 0;
+ SELECT AVG(weight_kg) AS average_weight FROM animals;
+ Select neutered, SUM(escape_attempts) FROM animals GROUP BY neutered;
+ SELECT MIN(weight_kg) AS min_weight, MAX(weight_kg) AS max_weight FROM animals;
+ SELECT AVG(escape_attempts) FROM animals WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31';
