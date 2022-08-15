1695e698acdbc8a01831e64fa2f03faa5fb130c8
MoRaad97 <89518488+MoRaad97@users.noreply.github.com>
2022-08-15 14:09:36 +0300

    Required Changes.

--- queries.sql
+++ queries.sql
@@ -9,7 +9,37 @@ SELECT * FROM animals WHERE neutered = true;
 SELECT * FROM animals WHERE name <> 'Gabumon';
 SELECT * FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
 
---part 2
+
+/* #1 Transaction Action*/
+BEGIN;
+UPDATE animals SET species = 'unspecified';
+SELECT * FROM animals;
+ROLLBACK;
+
+/* #2 Transaction Action*/
+BEGIN;
+UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
+UPDATE animals SET species = 'pokemon' WHERE species = '';
+SELECT * FROM animals;
+COMMIT;
+
+/* #3 Transaction Action*/
+BEGIN;
+DELETE FROM animals;
+ROLLBACK;
+SELECT * FROM animals;
+
+/* #4 Transaction Action*/
+BEGIN;
+DELETE FROM animals WHERE date_of_birth > '2022-01-01';
+SAVEPOINT SP1;
+UPDATE animals SET weight_kg = "weight_kg" * -1;
+ROLLBACK TO SAVEPOINT SP1;
+UPDATE animals SET weight_kg = "weight_kg" * -1 WHERE weight_kg < 0;
+SELECT * FROM animals;
+COMMIT;
+
+
  Select COUNT(*) AS number_of_animals FROM animals;
  Select escape_attempts, COUNT(*) FROM animals GROUP BY escape_attempts HAVING escape_attempts = 0;
  SELECT AVG(weight_kg) AS average_weight FROM animals;
